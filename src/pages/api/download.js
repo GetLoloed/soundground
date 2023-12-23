733c5fde991702322087fbaf1ae5225606d56d90
lolo <loic.aviez91@gmail.com>
2023-12-23 22:57:00 +0100

    Improve readability of download API with comments
    Enhanced the readability and maintenance of the download API by adding explanatory comments. Comments were added to clarify the purpose and functionality of imported modules

--- src/pages/api/download.js
+++ src/pages/api/download.js
@@ -1,3 +1,4 @@
+// Import the required modules
 import {pipeline} from 'stream';
 import ytdl from 'ytdl-core';
 import scdl from 'soundcloud-downloader';
@@ -9,6 +10,7 @@ import ffprobePath from '@ffprobe-installer/ffprobe';
 ffmpeg.setFfmpegPath(ffmpegPath.path);
 ffmpeg.setFfprobePath(ffprobePath.path);
 
+// Function to determine the service of the url (Youtube or Soundcloud)
 const getService = (url) => {
     if (ytdl.validateURL(url)) {
         return 'youtube';
@@ -23,12 +25,14 @@ export default async (req, res) => {
     try {
         const {url} = req.query;
 
+        // Validate if url is provided
         if (!url || typeof url !== 'string') {
             return res.status(400).json({error: 'URL not provided or not a string'});
         }
 
         const service = getService(url);
 
+        // Validate if url is from supported service
         if (!service) {
             return res.status(400).json({error: 'Invalid URL. Please enter a YouTube or SoundCloud URL.'});
         }
@@ -38,20 +42,22 @@ export default async (req, res) => {
         if (service === 'youtube') {
             const info = await ytdl.getInfo(url);
             title = info.videoDetails.title;
+            // Stream only the audio from the video
             stream = ytdl.downloadFromInfo(info, {quality: 'highestaudio'});
+            // Convert the audio to mp3 format
             stream = ffmpeg(stream).format('mp3').pipe();
         } else if (service === 'soundcloud') {
             const trackInfo = await scdl.getInfo(url);
             title = trackInfo.title;
             const data = await scdl.download(url);
-            stream = ffmpeg()
-                .input(data)
-                .format('mp3')
-                .pipe();
+            // Convert the audio to mp3 format
+            stream = ffmpeg().input(data).format('mp3').pipe()
         }
 
+        // Set the content disposition header so the browser prompts the user to download the file
         res.setHeader('Content-Disposition', `attachment; filename="${encodeURIComponent(title)}.mp3"`);
 
+        // Pipe the transformed stream to the response
         pipeline(stream, res, (error) => {
             if (error) {
                 console.error('Pipeline error:', error);
@@ -59,6 +65,7 @@ export default async (req, res) => {
             }
         });
     } catch (error) {
+        // Log the error and send a response
         console.error(error);
         res.status(500).json({error: 'An error occurred while processing your request.'});
     }
