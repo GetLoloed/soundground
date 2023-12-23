f0ad37d0f92c4ed27a2fd9158fb7c0aa87ab8053
lolo <loic.aviez91@gmail.com>
2023-12-24 00:20:47 +0100

    Exclude 'pngquant-bin' from client-side bundles
    Added 'pngquant-bin' to externals in webpack configuration for non-server bundles to optimize client-side performance. This adjustment ensures that server-only modules aren't needlessly sent to the client, improving load times and reducing unnecessary code transmission.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = [...config.externals, 'pngquant-bin'];
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
