c0479b29b8600aeb1e827a342139c12f7a591021
lolo <loic.aviez91@gmail.com>
2023-12-24 00:32:15 +0100

    Refactor API management function and error handling
    Refactored API management function in `download.js` to streamline download process and error handling. Updated error responses to utilize JSON format, improving readability and consistency. Also simplified code by removing excess imports and comments, resulting in a more efficient and clean structure.

--- src/pages/api/download.js
+++ src/pages/api/download.js
@@ -1,18 +1,14 @@
-// Importations nécessaires
-import { pipeline } from 'stream';
+import {pipeline} from 'stream';
 import ytdl from 'ytdl-core';
 import scdl from 'soundcloud-downloader';
 import ffmpeg from 'fluent-ffmpeg';
-
-// Définir les chemins pour ffmpeg et ffprobe en utilisant des variables d'environnement
-// ou inclure directement le binaire dans le projet et référencer son chemin relatif.
 import ffmpegPath from '@ffmpeg-installer/ffmpeg';
 import ffprobePath from '@ffprobe-installer/ffprobe';
 
+// Set ffmpeg and ffprobe paths
 ffmpeg.setFfmpegPath(ffmpegPath.path);
 ffmpeg.setFfprobePath(ffprobePath.path);
 
-// Fonction pour déterminer le service basé sur l'URL
 const getService = (url) => {
     if (ytdl.validateURL(url)) {
         return 'youtube';
@@ -23,18 +19,18 @@ const getService = (url) => {
     return null;
 };
 
-// Fonction de gestion API principale
 export default async (req, res) => {
     try {
-        const { url } = req.query;
+        const {url} = req.query;
 
         if (!url || typeof url !== 'string') {
-            return res.status(400).json({ error: 'URL not provided or not a string' });
+            return res.status(400).json({error: 'URL not provided or not a string'});
         }
 
         const service = getService(url);
+
         if (!service) {
-            return res.status(400).json({ error: 'Invalid URL. Please enter a YouTube or SoundCloud URL.' });
+            return res.status(400).json({error: 'Invalid URL. Please enter a YouTube or SoundCloud URL.'});
         }
 
         let stream;
@@ -42,28 +38,28 @@ export default async (req, res) => {
         if (service === 'youtube') {
             const info = await ytdl.getInfo(url);
             title = info.videoDetails.title;
-            stream = ytdl.downloadFromInfo(info, { quality: 'highestaudio' });
+            stream = ytdl.downloadFromInfo(info, {quality: 'highestaudio'});
+            stream = ffmpeg(stream).format('mp3').pipe();
         } else if (service === 'soundcloud') {
             const trackInfo = await scdl.getInfo(url);
             title = trackInfo.title;
-            stream = await scdl.download(url);
+            const data = await scdl.download(url);
+            stream = ffmpeg()
+                .input(data)
+                .format('mp3')
+                .pipe();
         }
 
-        // Configurer la réponse avec le bon en-tête pour le téléchargement du fichier
         res.setHeader('Content-Disposition', `attachment; filename="${encodeURIComponent(title)}.mp3"`);
-        ffmpeg(stream)
-            .format('mp3')
-            .on('end', () => {
-                console.log('Conversion finished!');
-            })
-            .on('error', (err) => {
-                console.error('Error:', err);
-                res.status(500).send('Error in processing your request');
-            })
-            .pipe(res, { end: true });
 
+        pipeline(stream, res, (error) => {
+            if (error) {
+                console.error('Pipeline error:', error);
+                res.status(500).send('Download failed.');
+            }
+        });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred while processing your request.' });
+        res.status(500).json({error: 'An error occurred while processing your request.'});
     }
-};
+}
