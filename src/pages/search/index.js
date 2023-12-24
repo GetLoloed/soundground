75b3a2e2c71072d9258af19aa927d56793a2583c
lolo <loic.aviez91@gmail.com>
2023-12-24 13:01:46 +0100

    Update search page and improve search result display
    This commit includes changes made to enhance search functionalities as well as the display of search results. Changes include renaming the preview URL state to "currentlyPlaying" and displaying more detailed information about each result including author name and source.

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,21 +1,20 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import Metadata from "@/components/Metadata";
 import Navbar from "@/components/Navbar";
 
 export default function Search() {
     const [query, setQuery] = useState("");
     const [results, setResults] = useState([]);
-    const [previewUrl, setPreviewUrl] = useState(null);
+    const [currentlyPlaying, setCurrentlyPlaying] = useState(null);
 
     const handleSearch = async () => {
-        // Replace with your actual search API endpoint
         const response = await fetch(`/api/search?query=${encodeURIComponent(query)}`);
         const data = await response.json();
         setResults(data.results);
     };
 
-    const handlePreview = async (url) => {
-        setPreviewUrl(`/api/preview?url=${encodeURIComponent(url)}`);
+    const handlePreview = (url) => {
+        setCurrentlyPlaying(url);
     };
 
     useEffect(() => {
@@ -44,17 +43,23 @@ export default function Search() {
                     </div>
                 </div>
                 {results.map(result => (
-                    <div key={result.id} className="bg-white text-black shadow-md p-4 md:p-8 mt-5 rounded-lg w-full max-w-xl flex flex-col md:flex-row items-start overflow-hidden">
-                        <img src={result.thumbnail} alt={result.title} className="w-full md:w-40 h-40 md:h-auto object-cover rounded-lg mb-4 md:mb-0 md:mr-5" />
+                    <div key={result.id}
+                         className="bg-white text-black shadow-md p-4 md:p-8 mt-5 rounded-lg w-full max-w-xl flex flex-col md:flex-row items-start overflow-hidden">
+                        <img src={result.thumbnail} alt={result.title}
+                             className="w-full md:w-40 h-40 md:h-auto object-cover rounded-lg mb-4 md:mb-0 md:mr-5"/>
                         <div>
-                            <h2 className="text-lg md:text-xl font-bold mb-2">{result.title}</h2>
+                            <h2 className="text-lg md:text-xl font-bold mb-2">{result.author} - {result.title}</h2>
+                            <p className="text-sm text-gray-500 mb-4">Source: {result.source}</p>
                             <div className="mt-4">
-                                <button onClick={() => handlePreview(result.url)} className="py-2 px-4 bg-black text-white rounded-md mr-2 transition-colors duration-200 transform hover:bg-blue-500">Preview</button>
-                                <a className="py-2 px-4 bg-black text-white rounded-md transition-colors duration-200 transform hover:bg-blue-500" href={`/api/download?url=${encodeURIComponent(result.url)}`}>Download</a>
+                                <button onClick={() => handlePreview(result.url)}
+                                        className="py-2 px-4 bg-black text-white rounded-md mr-2 transition-colors duration-200 transform hover:bg-blue-500">Preview
+                                </button>
+                                <a className="py-2 px-4 bg-black text-white rounded-md transition-colors duration-200 transform hover:bg-blue-500"
+                                   href={`/api/download?url=${encodeURIComponent(result.url)}`}>Download</a>
                             </div>
-                            {previewUrl && (
+                            {currentlyPlaying === result.url && (
                                 <audio controls className="mt-4">
-                                    <source src={previewUrl} type="audio/mpeg"/>
+                                    <source src={`/api/preview?url=${encodeURIComponent(result.url)}`} type="audio/mpeg"/>
                                     Your browser does not support the audio element.
                                 </audio>
                             )}
