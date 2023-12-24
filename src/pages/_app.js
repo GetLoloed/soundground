d7b21f571aaea5a9f1028b5760eda8ab6d24d087
lolo <loic.aviez91@gmail.com>
2023-12-24 16:37:53 +0100

    Remove gradient animation from _app.js and transfer to CSS
    The previous animation implementation made use of gsap in the _app.js React component, which has now been removed. Instead, the animation for the 'gradient' class has been defined directly in the global CSS file. This should result in cleaner code and more efficient animation performance.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,10 @@
 import '@/styles/globals.css'
 import {Analytics} from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { gsap } from 'gsap';
-import { useEffect } from 'react';
 
 export default function App({Component, pageProps}) {
-    useEffect(() => {
-        gsap.to('.gradient', {
-            background: 'linear-gradient(to bottom right, #1A202C, #000000)',
-            repeat: -1,
-            yoyo: true,
-            duration: 3,
-            ease: 'sine.inOut'
-        });
-    }, []);
-
     return (
-        <div className="gradient min-h-screen animate-gradient bg-gradient-to-r animation-3">
+        <div className="gradient min-h-screen">
             <Component {...pageProps} />
             <Analytics/>
             <SpeedInsights/>
