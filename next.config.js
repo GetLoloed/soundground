d0fba51dc74659526b6e2344d8b63892568e0aa0
lolo <loic.aviez91@gmail.com>
2023-12-24 01:31:54 +0100

    Remove `pngquant-bin` configuration from webpack
    The webpack configuration blocking 'pngquant-bin' has been removed. This simplifies the 'next.config.js' file, reducing unnecessary complexity since there's no longer a need to check the server status when building the webpack.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals = [...config.externals, 'pngquant-bin'];
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
